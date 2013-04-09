7f551b71947c48b9670c7b24bfd22ca7c5968d5c
hswayne77 <hswayne77@gmail.com>
2013-04-10 03:03:10 +0800

    Update vlan-tag.sh
    fix to if [ "${numTag}" == "0" ]

--- vlan-tag.sh
+++ vlan-tag.sh
@@ -11,12 +11,11 @@ function get_available_vlan_tag() {
     VLAN_TAG_LIST=$(xe vlan-list params=tag --minimal | awk 'BEGIN{FS=","} {for(i=1;i<NF;i++) print $i}' | sort -n | uniq)
     for tag in ${VLAN_TAG_LIST}
     do
-        #echo $i
         tagArr[$i]=${tag}
         let i++
     done
     local numTag=${#tagArr[@]}
-    if [ numTag == 0 ];then
+    if [ "${numTag}" == "0" ];then
         echo 1
     else
         index=$(expr ${#tagArr[@]} - 1)
