bf2ca78c6a11293637f2a0478556804abdadc046
hswayne77 <hswayne77@gmail.com>
2013-04-10 14:44:29 +0800

    Update vlan-tag.sh
    fix script description 

--- vlan-tag.sh
+++ vlan-tag.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-# author: winnie yang
+# [author] 
+#     winnie yang
 # [description]
 #     get a available vlan tag in Citrix Xenserver
+#     it will return a vlan tag in ascending order
 #
 
 
 function get_available_vlan_tag() {
-    #set -x
     local i=0
     VLAN_TAG_LIST=$(xe vlan-list params=tag --minimal | awk 'BEGIN{FS=","} {for(i=1;i<NF;i++) print $i}' | sort -n | uniq)
     for tag in ${VLAN_TAG_LIST}
@@ -21,7 +22,6 @@ function get_available_vlan_tag() {
         index=$(expr ${#tagArr[@]} - 1)
         echo $(expr ${tagArr[$index]} + 1)
     fi
-    #set +x
 }
 
 
